af3498c0feee92238297be31c6b6e9f9a9481b39
Rudy Becker <52896529+RudyBecker@users.noreply.github.com>
2021-04-06 19:06:43 -0700

    remove button from import (#25)

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/nav";
 import Image from "next/image";
-import { Button, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 export default function Home() {
   return (
