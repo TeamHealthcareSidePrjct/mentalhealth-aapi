7c9b39ad9131ffe6cc66e9e226dec7d6ada5ad2c
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2021-04-30 19:23:02 -0700

    fix nav font

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,19 @@
 import { Navbar, Nav } from "react-bootstrap";
+import styles from "../styles/Home.module.css";
 
 export default function NavBar() {
   return (
     <div>
       <Navbar className="navbar navbar-dark" bg="clear" expand="lg">
-        <Navbar.Brand href="/">Hyphenated</Navbar.Brand>
+        <Navbar.Brand className={styles.navbarstitlestyles} href="/">
+          Hyphenated
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="/about">About</Nav.Link>
+          <Nav className={"ml-auto pr-3"}>
+            <Nav.Link href="/about">
+              <h3>About</h3>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
